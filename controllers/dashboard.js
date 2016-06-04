e4e9402dbb92d3bc3be441ce6eb6ea0133b1927f
lielfr <lielft@gmail.com>
2016-06-04 15:08:17 +0300

    Integrated auth middleware and it's working fine so far.

--- controllers/dashboard.js
+++ controllers/dashboard.js
@@ -1,26 +1,49 @@
 var mongodb = require('mongodb').MongoClient;
 var express = require('express');
 var config = require('../config');
+var auth_middleware = require('../middleware/auth');
+
+
 var router = express.Router();
 
+router.use(auth_middleware);
 router.get('/', function(req, res) {
-  req.session.uid = req.user.id;
-  mongodb.connect(config.db.mongoURL, function(err, db) {
-    if (err) return console.error(err);
-
-    var profiles = db.collection('profiles');
-    profiles.insertOne({
-      uid: req.user.id,
-      displayName: req.user.displayName,
-      image: req.user.profile_image_url
-    }, function(err, result) {
+  if (!req.isAuthorized) {
+    req.session.uid = req.user.id;
+    mongodb.connect(config.db.mongoURL, function(err, db) {
       if (err) return console.error(err);
-      if (result.n !== 1)
-        console.log('Something is weird with '+req.user.id+'.');
-      db.close();
+      var profiles = db.collection('profiles');
+      profiles.insertOne({
+        uid: req.user.id,
+        displayName: req.user.displayName,
+        image: req.user.profile_image_url
+      }, function(err, result) {
+        if (err) return console.error(err);
+        if (result.insertedCount !== 1)
+          console.log('Something is weird with '+req.user.id+'.');
+        db.close();
+        res.end('Logged in. Type /dashboard/logout to sign out.');
+      });
     });
-  });
-  res.end('Done: '+JSON.stringify(req.user));
+  } else {
+    res.end('Welcome, '+req.userObj.displayName+'.');
+  }
+});
+
+router.get('/logout', function(req, res) {
+  if (!req.isAuthorized)
+    res.end('Not logged in.');
+  else {
+    mongodb.connect(config.db.mongoURL, function(err, db) {
+      if (err) return console.error(err);
+      var profiles = db.collection('profiles');
+      profiles.deleteOne({uid: req.session.uid}, function(err, result) {
+        if (err) return console.error(err);
+        delete req.session.uid;
+        res.end('Logged out.');
+      });
+    });
+  }
 });
 
 module.exports = router;
--- middleware/auth.js
+++ middleware/auth.js
@@ -2,28 +2,31 @@ var config = require('../config');
 var mongodb = require('mongodb').MongoClient;
 
 function authMiddleware(req, res, next) {
-  if (!req.session || !req.session.uid)
+  if (!req.session.uid) {
     req.isAuthorized = false;
+    next();
+  }
   else {
     mongodb.connect(config.db.mongoURL, function(err, db) {
-      if (error) return console.error(err);
+      console.log('Connected to database.');
+      if (err) return console.error(err);
 
       var profiles = db.collection('profiles');
       profiles.find({uid: req.session.uid}).toArray(function(err, docs) {
         if (err) return console.error(err);
-        if (docs.length == 0)
-          req.isAuthorized = false;
-        else if (docs.length > 1)
+        req.isAuthorized = docs.length === 1;
+        if (docs.length > 1)
           console.log('Something is going wrong with user '+req.session.uid+'.');
         else {
           req.isAuthorized = true;
           req.userObj = docs[0];
         }
         db.close();
+        next();
       });
     });
   }
-  next();
+
 }
 
 module.exports = authMiddleware;
